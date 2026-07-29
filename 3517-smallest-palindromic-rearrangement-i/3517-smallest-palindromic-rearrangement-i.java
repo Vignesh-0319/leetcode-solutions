924f69d19777b1d6783bea5b92c970a9e83c48dd
Vignesh-0319 <vignesh0319.becse24@chitkara.edu.in>
2026-07-29 22:42:13 +0530

    Time: 23 ms (82.4%), Space: 48.3 MB (37.6%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -1,6 +1,5 @@
 class Solution {
     public String smallestPalindrome(String s) {
-        int N = s.length();
         char[] input = s.toCharArray();
         
         int[] count = new int[26];
@@ -9,18 +8,18 @@ class Solution {
             count[c-'a']++;
         
         int j=0;
-        char[] output = new char[N];
+        char[] output = new char[s.length()];
         for (int i=0;i<26;i++){
             int f = count[i];            
             char curr = (char) ('a' + i);
             while (f>1){
                 output[j] = curr;
-                output[N-1-j] = curr;
+                output[s.length()-1-j] = curr;
                  f -= 2;
                  j++;
             }
             if (f==1)
-                output[N/2] = curr;            
+                output[s.length()/2] = curr;            
         }
         return new String(output);
     }
