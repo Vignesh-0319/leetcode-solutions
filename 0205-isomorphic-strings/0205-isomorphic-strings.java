12cdd65dd6022109d4fc12785a9c8b1596b00caf
Vignesh-0319 <vignesh0319.becse24@chitkara.edu.in>
2026-07-29 21:58:14 +0530

    Time: 7 ms (80.69%), Space: 43.9 MB (42.41%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
+
         int[] mapS = new int[256];
         int[] mapT = new int[256];
         for (int i = 0; i < s.length(); i++) {
