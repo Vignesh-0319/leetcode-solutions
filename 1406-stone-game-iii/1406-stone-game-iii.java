ad131ae72ea0df1ff987dfe53557843b179a5bb2
Vignesh-0319 <vignesh0319.becse24@chitkara.edu.in>
2026-08-03 20:16:44 +0530

    Time: 10 ms (83.24%), Space: 85.5 MB (71.59%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -1,30 +1,23 @@
 class Solution {
     public String stoneGameIII(int[] s) {
-        Integer[] dp = new Integer[s.length];
-        int ans = chk(s, 0, dp);
+        int n = s.length;
+        int[] dp = new int[n + 3];
 
-        if (ans > 0) return "Alice";
-        if (ans < 0) return "Bob";
+        for (int i = n - 1; i >= 0; i--) {
+
+            int take = 0;
+            dp[i] = Integer.MIN_VALUE;
+
+            for (int k = 0; k < 3 && i + k < n; k++) {
+                take += s[i + k];
+                dp[i] = Math.max(dp[i], take - dp[i + k + 1]);
+            }
+        }
+
+        if (dp[0] > 0)
+            return "Alice";
+        if (dp[0] < 0)
+            return "Bob";
         return "Tie";
-    }
-
-    private int chk(int[] s, int i, Integer[] dp) {
-        if (i >= s.length)
-            return 0;
-
-        if (dp[i] != null)
-            return dp[i];
-
-        int t1 = s[i] - chk(s, i + 1, dp);
-
-        int t2 = Integer.MIN_VALUE;
-        if (i + 1 < s.length)
-            t2 = s[i] + s[i + 1] - chk(s, i + 2, dp);
-
-        int t3 = Integer.MIN_VALUE;
-        if (i + 2 < s.length)
-            t3 = s[i] + s[i + 1] + s[i + 2] - chk(s, i + 3, dp);
-
-        return dp[i] = Math.max(t1, Math.max(t2, t3));
     }
 }
