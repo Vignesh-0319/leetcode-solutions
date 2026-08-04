4a826dbe53a1f2792f4f58a5571c8884cab6583e
Vignesh-0319 <vignesh0319.becse24@chitkara.edu.in>
2026-08-04 13:25:45 +0530

    Time: 0 ms (100%), Space: 42 MB (66.65%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        Integer dp[][] = new Integer[m][n];
+        return chk(m-1,n-1,dp);
+    }
+    private int chk(int m, int n,Integer [][]dp){
+        if(m==0&&n==0) return 1;
+        if(0>m||0>n) return 0;
+        if(dp[m][n]!=null) return dp[m][n];
+        return dp[m][n] = chk(m-1,n,dp)+chk(m,n-1,dp);
+    }
+}
