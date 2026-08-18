40f792aa9fb1c16935558504147fc5683e426477
Vignesh-0319 <vignesh0319.becse24@chitkara.edu.in>
2026-08-18 23:31:56 +0530

    Time: 7 ms (65.3%), Space: 47.1 MB (14.26%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,19 +1,61 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         int sum = 0;
-        for(int num : nums){
-            sum+=num;
+
+        for (int num : nums) {
+            sum += num;
         }
-        if(Math.abs(target)>sum) return 0;
-        Integer [][]dp = new Integer[nums.length+1][2*sum+1];
-        return sol(nums, nums.length, target, dp, 0, sum);
+
+        // Target is outside possible range
+        if (Math.abs(target) > sum) {
+            return 0;
+        }
+
+        int offset = sum;
+
+        Integer[][] dp = new Integer[nums.length + 1][2 * sum + 1];
+
+        return sol(nums, nums.length, target, dp, 0, offset);
     }
-    private int sol(int[] nums, int i, int target, Integer[][] dp, int val, int offset){
-        if(i==0){
-            if(val==target) return 1;
-            else return 0;
+
+    private int sol(int[] nums, int i, int target,
+                    Integer[][] dp, int val, int offset) {
+
+        // Base case
+        if (i == 0) {
+            return val == target ? 1 : 0;
         }
-        if(dp[i][val + offset]!=null) return dp[i][val + offset];
-        return dp[i][val+offset] = sol(nums, i-1,target, dp, val+nums[i-1],offset)+sol(nums, i-1,target, dp, val-nums[i-1],offset);
+
+        // Already calculated
+        if (dp[i][val + offset] != null) {
+            return dp[i][val + offset];
+        }
+
+        int num = nums[i - 1];
+
+        // Choose +
+        int add = sol(
+            nums,
+            i - 1,
+            target,
+            dp,
+            val + num,
+            offset
+        );
+
+        // Choose -
+        int subtract = sol(
+            nums,
+            i - 1,
+            target,
+            dp,
+            val - num,
+            offset
+        );
+
+        // Store answer
+        dp[i][val + offset] = add + subtract;
+
+        return dp[i][val + offset];
     }
 }
