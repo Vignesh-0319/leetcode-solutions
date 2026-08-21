2e8111dad4f3b8eeb600bb5da84c1f10be52f8d4
Vignesh-0319 <vignesh0319.becse24@chitkara.edu.in>
2026-08-21 20:09:49 +0530

    Time: 2 ms (73.05%), Space: 44.9 MB (97.13%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] finalPrices(int[] p) {
+        int arr[] = new int[p.length];
+        for(int i=0; i<p.length; i++){
+            arr[i]=p[i];
+            for(int j=i+1; j<p.length; j++){
+                if(p[i]>=p[j]){
+                    arr[i]=p[i]-p[j];
+                    break;
+                }
+            }
+        }
+        return arr;
+    }
+}
