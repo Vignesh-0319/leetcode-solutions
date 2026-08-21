1140d98acaff7d245281a019115a7c832846b5cc
Vignesh-0319 <vignesh0319.becse24@chitkara.edu.in>
2026-08-21 14:10:45 +0530

    Time: 30 ms (91.85%), Space: 48.5 MB (69.59%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.java
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -1,8 +1,8 @@
 class Solution {
     public int distributeCandies(int[] ca) {
-        HashSet<Integer> s = new HashSet<>();
-        for(int i =0; i<ca.length; i++){
-            s.add(ca[i]);
+        Set<Integer> s = new HashSet<>();
+        for(int i : ca){
+            s.add(i);
         }
         return Math.min(s.size(), ca.length/2); 
     }
