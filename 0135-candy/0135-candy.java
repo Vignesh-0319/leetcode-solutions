73212b0f4411fcf2d73138ab176f1b3d15bde73f
Vignesh-0319 <vignesh0319.becse24@chitkara.edu.in>
2026-08-21 23:50:36 +0530

    Time: 3 ms (86.31%), Space: 52.4 MB (5.37%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -10,12 +10,16 @@ class Solution {
                 candies[i] = candies[i - 1] + 1;
             }
         }
-        int total = candies[n-1];
+
         for (int i = n - 2; i >= 0; i--) {
             if (ratings[i] > ratings[i + 1]) {
                 candies[i] = Math.max(candies[i], candies[i + 1] + 1);
             }
-            total += candies[i];
+        }
+
+        int total = 0;
+        for (int c : candies) {
+            total += c;
         }
         return total;
     }
